e24a8ead02a691ff1a8ed2e223cc9374f6baea80
lauichan <te06230623@gmail.com>
2024-03-06 14:59:46 +0900

    feat: form 추가

---
+++ src/components/todo/TodoForm.tsx
@@ -0,0 +1,17 @@
+import styles from "./TodoForm.module.css";
+
+type TodoFormProps = {
+  handleAddTodo: (e: React.FormEvent<HTMLFormElement>) => void;
+};
+
+function TodoForm({ handleAddTodo }: TodoFormProps) {
+  return (
+    <form className={styles.form} onSubmit={handleAddTodo}>
+      <input name="heading" placeholder="할 일"></input>
+      <textarea name="content" placeholder="내용"></textarea>
+      <button>추가</button>
+    </form>
+  );
+}
+
+export default TodoForm;
