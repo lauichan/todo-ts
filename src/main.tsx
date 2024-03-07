3a6ff1a29187cbf5d29ee0b132e5a16568e9eee4
lauichan <te06230623@gmail.com>
2024-03-07 16:27:24 +0900

    chore: 안쓰는 패키지 redux toolkit 제거

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./store/config/configStore.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </QueryClientProvider>
 );
