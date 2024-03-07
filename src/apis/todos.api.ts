86aea0b0a6dfd6c1f24d922993979cbedd7e7dee
lauichan <te06230623@gmail.com>
2024-03-07 19:55:41 +0900

    refactor: api 분리

---
+++ src/apis/todos.api.ts
@@ -0,0 +1,22 @@
+import { todoAPI } from "./todos";
+import { NewTodo, Todo } from "../types/todo";
+
+export const getTodos = async (): Promise<Todo[]> => {
+  const response = await todoAPI.get("/todos");
+  return response.data;
+}
+
+export const addTodosApi = async (newTodos: NewTodo): Promise<Todo> => {
+  const response = await todoAPI.post("/todos", newTodos);
+  return response.data;
+}
+
+export const updateTodosApi = async (updateTodos: Todo): Promise<Todo> => {
+  const response = await todoAPI.patch(`/todos/${updateTodos.id}`, updateTodos);
+  return response.data;
+}
+
+export const deleteTodosApi = async (id: string):  Promise<Todo> => {
+  const response = await todoAPI.delete(`/todos/${id}`);
+  return response.data;
+}
