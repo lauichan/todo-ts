a9154492e04ac214a3dca6a6c6d56ea0785b777d
lauichan <te06230623@gmail.com>
2024-03-07 16:34:37 +0900

    remove: 안쓰는 redux 파일 제거

--- src/store/config/configStore.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todoSlice from "../modules/todoSlice";
-
-const store = configureStore({reducer : { todos: todoSlice }})
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-
-export default store;
