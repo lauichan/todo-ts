d3040dc6dbfafe433c5770c72067883b53af89f8
lauichan <te06230623@gmail.com>
2024-03-07 16:27:53 +0900

    chore: redux provider 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { Provider } from "react-redux";
-import store from "./store/config/configStore.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </QueryClientProvider>
 );
